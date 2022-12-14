b1a3b12f39debca17c5509e32794d2115855da9e
nanoiris <y.ding@knights.ucf.edu>
2022-12-14 10:58:51 -0500

    upload all files for this finished project

---
+++ src/main/java/com/tradez/TradezApplication.java
@@ -0,0 +1,15 @@
+package com.tradez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class TradezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradezApplication.class, args);
+	}
+
+}
